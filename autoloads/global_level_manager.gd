fbbe19ce3b7b6dedf4d4214945151fe479b55484
philozofer <giovan.major@gmail.com>
2024-07-08 15:36:49 -0400

    created basic scene_manager + scene transitions

--- autoloads/global_level_manager.gd
+++ autoloads/global_level_manager.gd
@@ -1,9 +1,49 @@
 extends Node
 
-var current_tilemap_bounds: Array[Vector2]
+
+signal level_load_started
+signal level_loaded
 signal TileMapBoundsChanged(bounds : Array[Vector2])
+
+
+
+var current_tilemap_bounds: Array[Vector2]
+var target_transition: String
+var position_offset: Vector2
+
+
+func _ready():
+	await get_tree().process_frame
+	level_loaded.emit()
 
 func changeTilemapBounds(bounds: Array[Vector2]) -> void:
 	current_tilemap_bounds = bounds
 	TileMapBoundsChanged.emit(bounds)
+
+func load_new_level(
+	level_path: String,
+	_target_transition: String,
+	_postition_offset: Vector2
+) -> void:
+	
+	get_tree().paused = true
+	target_transition = _target_transition
+	position_offset = _postition_offset
+	
+	await get_tree().process_frame # Level Transition (effects)
+	
+	level_load_started.emit()
+	
+	await get_tree().process_frame
+	
+	get_tree().change_scene_to_file(level_path)
+	
+	await get_tree().process_frame # Level Transition (effects)
+	
+	get_tree().paused = false
+	
+	await get_tree().process_frame
+	
+	level_loaded.emit()
+	
 	pass
--- scripts/levels/level.gd
+++ scripts/levels/level.gd
@@ -5,6 +5,10 @@ class_name Level extends Node2D
 func _ready():
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent(self)
-	pass # Replace with function body.
+	LevelManager.level_load_started.connect(_free_level)
 
 
+func _free_level() -> void:
+	PlayerManager.unparent_player(self)
+	queue_free()
+	pass
