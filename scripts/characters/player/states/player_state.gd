0c8fd0e8ba7d42e8b56edeac1c5b26bc73628fd6
philozofer <giovan.major@gmail.com>
2024-07-08 09:56:53 -0400

    added player stun state

--- scripts/characters/player/states/player_state.gd
+++ scripts/characters/player/states/player_state.gd
@@ -2,11 +2,16 @@ class_name State extends Node
 
 ## Stores a reference to the player that this State belongs to
 static var player: Player
+static var state_machine: PlayerStateMachine
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func init() -> void:
+	pass
+	
 ## What happens when the player enters this State ?
 func enter() -> void:
 	pass
--- scripts/characters/player/states/player_state_machine.gd
+++ scripts/characters/player/states/player_state_machine.gd
@@ -27,11 +27,18 @@ func initialize(_player: Player) -> void:
 		if c is State:
 			states.append(c)
 	
-	if states.size() > 0:
-		states[0].player = _player
-		change_state(states[0])
-		process_mode = Node.PROCESS_MODE_INHERIT
-	return
+	if states.size() == 0:
+		return		
+	
+	states[0].player = _player
+	states[0].state_machine = self
+	
+	for state in states:
+		state.init()
+	
+	change_state(states[0])
+	process_mode = Node.PROCESS_MODE_INHERIT
+	
 
 func change_state(new_state: State) -> void:	
 	if new_state == null || new_state == current_state:
