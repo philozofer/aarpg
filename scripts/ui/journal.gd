050fc9cb4318ceb878b536fa23cf09039bc20b28
philozofer <giovan.major@gmail.com>
2024-07-14 10:31:02 -0400

    added quest_menu signal and change of quest_desc

--- scripts/ui/journal.gd
+++ scripts/ui/journal.gd
@@ -1,32 +1,27 @@
 extends Panel
 
-@onready var journal: Panel = $"."
-@onready var quest_list: ItemList = $QuestList
-@onready var quest_description: RichTextLabel = $QuestDescription
+signal quest_selected
 
-@onready var quest_selected: Quest
+@onready var quest_list: ItemList = $QuestList
+@onready var quest_description: RichTextLabel = $ColorRect2/Control/QuestDescription
+
+func _ready():
+	connect("quest_selected", _on_quest_list_item_selected )
+	pass
 
 func update_journal() -> void:	
-	quest_list.clear()
-	
+	quest_list.clear() 	
+		
 	if QuestSystem.get_active_quests().size() > 0:			
-		for i in QuestSystem.get_active_quests():
-			print(i.quest_name)
-			quest_list.add_item(i.quest_name)
-			pass
+		var active_quests = QuestSystem.get_active_quests()
+		for quest in active_quests:			
+			quest_list.add_item(quest.quest_name)	
+		quest_description.text = ""		
+		_on_quest_list_item_selected(0)	
 	
-	quest_description.text = ""	
-	_on_journal_item_selected(0)
-	quest_list.select(0)
-	
-	
-func _on_journal_item_selected(index: int) -> void:
-	# Assuming you want to select the first item:
-	var first_item_index = quest_list.get_item_at_position(Vector2(0, 0))
-	var selected_quest = quest_list.get_item(first_item_index)
-	#quest_description.text = selected_quest.quest_description	
-	update_quest_description(selected_quest.quest_description)	
-	pass
+func _on_quest_list_item_selected(_index: int) -> void:	
+	var selected_quest_id = QuestSystem.get_active_quests()[_index]
+	update_quest_description(QuestSystem.get_active_quests()[_index].quest_description)	
 	
 func update_quest_description(new_text: String) -> void:
 	quest_description.text = new_text
